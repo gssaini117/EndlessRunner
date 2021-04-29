31369e4eaf3dfa98cf762f6330c77cd4849cf652
Kushiri666 <Hewdraw123@gmail.com>
2021-04-29 01:36:49 -0700

    Post credits commit

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -4,10 +4,14 @@ class Credits extends Phaser.Scene {
     }
 
     preload() {
-        
+        // load background
+        this.load.image('creditsBackground', './assets/Menu_Credits.png');
     }
 
     create() {
+        this.background = this.add.tileSprite(
+            0, 0, 1024, 576, 'creditsBackground'
+        ).setOrigin(0, 0).setDepth(0);
 
 
         keyEsc = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
